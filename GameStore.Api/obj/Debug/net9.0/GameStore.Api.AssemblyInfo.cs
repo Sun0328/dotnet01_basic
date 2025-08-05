179ddfd4c0c28c989c168a420bcc8d6076192cdd
Sun0328 <104556029+Sun0328@users.noreply.github.com>
2025-08-05 15:47:34 +1200

    add feature completed

--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d91cd67eee5b69b362b0b705c23996fc90ccb6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f79bce1f9c8eaf0fed8eaada8b17b1384499b84d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
